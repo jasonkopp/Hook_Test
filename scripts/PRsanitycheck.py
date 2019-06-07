eed8c3502bd58b1cea3e3ae23af83c2428d0617f
Jason Kopp <j_kopp@apple.com>
2019-06-07 10:47:08 -0700

    notfourcharactercodes is operational. Testing with jason code.

--- scripts/PRsanitycheck.py
+++ scripts/PRsanitycheck.py
@@ -1,18 +1,17 @@
 #!/usr/bin/env python3
 import csv, re, os
 
-# are all the codes actually 4 characters (or an escape)?
+# X- are all the codes actually 4 characters (or an escape)?
 # are the codes unique, at least in their table, (and give a warning if the code is also in another table)
 # are the spec shortnames registered, or will they be?
 # are all the mandatory columns filled in?
 
-def csv4ccs():
-    CSVFileDirectory = "../CSV/"
+def getCSV4CCs(directory):
     csvCodes = []
     codesInCSV = []
-    for fileName in os.listdir(CSVFileDirectory):
+    for fileName in os.listdir(directory):
         if fileName.endswith(".csv") and fileName != "oti.csv" and fileName != "stream-types.csv":
-            with open(CSVFileDirectory+fileName, 'r') as csvfile:
+            with open(directory+fileName, 'r') as csvfile:
                 csvReader = csv.DictReader(csvfile)
                 headers = csvReader.fieldnames
                 if 'code' in headers:
@@ -28,18 +27,29 @@ def csv4ccs():
 
                         csvCodes.append(csvCode)
                         codesInCSV.append([csvCode, csvSpec, csvFile, csvLine])
+    return codesInCSV
 
+def notfourcharacters(codes, exceptions=[""]):
     pattern = re.compile("^[A-Za-z0-9 +-]{4}$")
-    exceptions = ["gif","png","tga"]
-    for code in codesInCSV:
+    mistakeCodes = []
+    for code in codes:
         if pattern.match(code[0]) == None:
             if code[0] not in exceptions:
-                print(code[0])
-
-    print("There are %d CSV Codes" % (len(csvCodes)))
+                mistakeCodes.append(code[0])
+    if mistakeCodes == []:
+        print("All 4ccs are four characters.")
+        return 0
+    else:
+        print("Something is wrong with these codes: %s" % mistakeCodes)
+        return 1
 
 def prsanitycheck():
-    csv4ccs()
-    exit(0)
+    codesInCSV = getCSV4CCs("../CSV/")
+    exceptions = ["gif","png","tga"]
+    mistakes = notfourcharacters(codesInCSV, exceptions)
+    if mistakes == 0:
+        exit(0)
+    elif mistakes != 0:
+        exit(1)
 
 prsanitycheck()
