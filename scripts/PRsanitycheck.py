dd8a431068be948f4f3825e13650f2ca0ed77b8c
Jason Kopp <j_kopp@apple.com>
2019-06-11 10:52:01 -0700

    travirepo

--- scripts/PRsanitycheck.py
+++ scripts/PRsanitycheck.py
@@ -90,7 +90,7 @@ def prsanitycheck():
     #GET CODES
     localrepo = "../CSV/"
     travisrepo = "CSV/"
-    codesspecs = getCSV4CCs(localrepo)
+    codesspecs = getCSV4CCs(travisrepo)
 
     #TEST for four characters
     codeExceptions = [] #Type in exceptions if you need to
